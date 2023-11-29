cbe2153a8095e9687de03d1bf10db641c10810b1
dljinook <142816076+dljinook@users.noreply.github.com>
2023-11-29 10:18:49 +0900

    Update PostList.jsx

--- src/pages/post/PostList.jsx
+++ src/pages/post/PostList.jsx
@@ -45,6 +45,7 @@ function PostList() {
   useEffect(() => {
     postData();
     console.log(totalDataLength);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
   return (
